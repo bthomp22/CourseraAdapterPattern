57b6cbe86feda8c287fd5b2c42286fd9968777cb
bthomp22 <brentthompson2012@gmail.com>
2020-10-27 08:59:12 -0400

    Confused Commit

---
+++ src/OldCoffeeMachine.java
@@ -0,0 +1,9 @@
+
+public class OldCoffeeMachine {
+	public void SelectA() {
+		System.out.println("Serving coffee from slot A");
+	}
+	public void SelectB() {
+		System.out.println("Serving coffee from slot B");
+	}
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		OldCoffeeMachine old = new OldCoffeeMachine();
+		CoffeeTouchScreenAdapter adapter = new CoffeeTouchScreenAdapter();
+		old.SelectA();
+	}
+
+}
